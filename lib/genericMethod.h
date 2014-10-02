a2adc180b97069d64b58a5891d9f8328fb496297
Geraldo Braz <eng.geraldo.braz@gmail.com>
2014-10-01 22:25:31 -0300

    genericMethods.h alterada

--- lib/genericMethod.h
+++ lib/genericMethod.h
@@ -3,26 +3,31 @@ public:
 	GenericMethod(double aproximacaoInicial, double a, double E1, double E2);
 
 	void init();
-	void testeParadaE1();
-	void testeParadaE2();
+	bool testeParadaE1();
+	bool testeParadaE2();
 	void setNovaIteracao();
 	void salvaEmLista();
 	void loop();
 	void show();
 
 
-
-
 //-----Getters and Setters
 
+	int getIterationsNumber();
+	double getA();
 	double getE1();
 	double getE2();
 
+	void setIterationsNumber(int intNumber);
+	void setA(double A);
 	void setE1(double E1);
 	void setE2(double E2);
 
 private:
+	int iterationsNumber;
+	double A;
 	double E1;
 	double E2;
-
+	double *iterationResults
+	ListResults *allResults
 }
